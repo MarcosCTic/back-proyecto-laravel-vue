d3d6111d796814dc5b1798d16c3c51113ce3f7d5
MarcosCTic <mcfoxtic@gmail.com>
2025-01-22 17:17:58 -0400

    migraciones

---
+++ database/migrations/2025_01_22_103633_create_categorias_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categorias', function (Blueprint $table) {
+            $table->id(); // PK, AUTO INCREMENT, BigInt, Unsigned
+            $table->string("nombre", 30)->unique();
+            $table->text("detalle")->nullable();
+            
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categorias');
+    }
+};
